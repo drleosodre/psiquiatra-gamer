fa02677a731d2243cc29c1d160d7225df93b7750
drleosodre <leo.sodre@gmail.com>
2026-08-13 09:29:12 -0300

    Publica post multiplayer-toxico (EN)

--- en/sidebar.js
+++ en/sidebar.js
@@ -2,6 +2,7 @@
    When publishing a new post, add an item to the top of the POSTS array below. */
 (function(){
   var POSTS = [
+    {s:"multiplayer-toxico", t:"Toxic multiplayer: why we become someone else online", g:null, c:"cultura"},
     {s:"escolhas-morais-culpa", t:"Moral choices in games: guilt, reparation, and Klein's mind", g:null, c:"conceitos"},
     {s:"terror-por-que-gostamos-de-medo", t:"Why we like to feel afraid in horror games", g:null, c:"conceitos"},
     {s:"nostalgia-jogos-retro", t:"Retro-game nostalgia: why the past is such a comfort", g:null, c:"conceitos"},
