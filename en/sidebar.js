869bf8759bfcb9b6908201f391e5311701c0bc53
Robo Psiquiatra Gamer <actions@github.com>
2026-09-08 17:37:13 +0000

    Publica post dark-souls (PT + EN)

--- en/sidebar.js
+++ en/sidebar.js
@@ -2,6 +2,7 @@
    When publishing a new post, add an item to the top of the POSTS array below. */
 (function(){
   var POSTS = [
+    {s:"dark-souls", t:"Dark Souls and the compulsion to repeat: why we insist on dying", g:null, c:"analises"},
     {s:"colecionismo-vs-toc", t:"Collecting or disorder? When hoarding games becomes a problem", g:null, c:"conceitos"},
     {s:"biblioteca-gigante-backlog", t:"Full library, nothing to play: the desire to own and the boredom of having", g:null, c:"conceitos"},
     {s:"imersao-perdida-adulto", t:"Why can't I lose myself in a game like I did as a kid?", g:null, c:"conceitos"},
